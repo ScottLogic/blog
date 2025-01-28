47f74a619dd800d50647cd35f7cc3db771c9333d
Chris Wilton-Magras <chriswilty@zoho.com>
2025-01-28 08:23:29 +0000

    Disable chrome sandbox SUID shenanigans

--- .pa11yci.js
+++ .pa11yci.js
@@ -21,6 +21,7 @@ module.exports = {
   defaults: {
     chromeLaunchConfig: {
       executablePath: chromiumBin,
+      "args": ["--disable-setuid-sandbox"],
     },
     ignore: [
       ...colourContrastRuleIds,
